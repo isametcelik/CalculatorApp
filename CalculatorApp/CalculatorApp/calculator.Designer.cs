0dae1a09db6cd44b6770e40f20296dc91c0e08a4
İsmail Samet Çelik <i.samet.celik@outlook.com>
2024-05-20 00:47:17 +0300

    CalculatorApp
    Calculator application developed with C# form

---
+++ CalculatorApp/CalculatorApp/calculator.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace CalculatorApp
+{
+	partial class calculator
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TxtBox = new System.Windows.Forms.TextBox();
+			this.yedibtn = new System.Windows.Forms.Button();
+			this.dortbtn = new System.Windows.Forms.Button();
+			this.birbtn = new System.Windows.Forms.Button();
+			this.sıfırbtn = new System.Windows.Forms.Button();
+			this.vrglbtn = new System.Windows.Forms.Button();
+			this.ikibtn = new System.Windows.Forms.Button();
+			this.besbtn = new System.Windows.Forms.Button();
+			this.sekizbtn = new System.Windows.Forms.Button();
+			this.esitbtn = new System.Windows.Forms.Button();
+			this.ucbtn = new System.Windows.Forms.Button();
+			this.altibtn = new System.Windows.Forms.Button();
+			this.dokuzbtn = new System.Windows.Forms.Button();
+			this.yuzdebtn = new System.Windows.Forms.Button();
+			this.carpbtn = new System.Windows.Forms.Button();
+			this.artibtn = new System.Windows.Forms.Button();
+			this.temizlebtn = new System.Windows.Forms.Button();
+			this.artieksibtn = new System.Windows.Forms.Button();
+			this.bolmebtn = new System.Windows.Forms.Button();
+			this.eksibtn = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// TxtBox
+			// 
+			this.TxtBox.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.TxtBox.Location = new System.Drawing.Point(13, 13);
+			this.TxtBox.Name = "TxtBox";
+			this.TxtBox.Size = new System.Drawing.Size(480, 52);
+			this.TxtBox.TabIndex = 0;
+			this.TxtBox.Text = "0";
+			this.TxtBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// yedibtn
+			// 
+			this.yedibtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.yedibtn.Location = new System.Drawing.Point(12, 80);
+			this.yedibtn.Name = "yedibtn";
+			this.yedibtn.Size = new System.Drawing.Size(77, 88);
+			this.yedibtn.TabIndex = 1;
+			this.yedibtn.Text = "7";
+			this.yedibtn.UseVisualStyleBackColor = true;
+			this.yedibtn.Click += new System.EventHandler(this.yedibtn_Click);
+			// 
+			// dortbtn
+			// 
+			this.dortbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.dortbtn.Location = new System.Drawing.Point(12, 174);
+			this.dortbtn.Name = "dortbtn";
+			this.dortbtn.Size = new System.Drawing.Size(77, 88);
+			this.dortbtn.TabIndex = 2;
+			this.dortbtn.Text = "4";
+			this.dortbtn.UseVisualStyleBackColor = true;
+			this.dortbtn.Click += new System.EventHandler(this.dortbtn_Click);
+			// 
+			// birbtn
+			// 
+			this.birbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.birbtn.Location = new System.Drawing.Point(12, 268);
+			this.birbtn.Name = "birbtn";
+			this.birbtn.Size = new System.Drawing.Size(77, 88);
+			this.birbtn.TabIndex = 3;
+			this.birbtn.Text = "1";
+			this.birbtn.UseVisualStyleBackColor = true;
+			this.birbtn.Click += new System.EventHandler(this.birbtn_Click);
+			// 
+			// sıfırbtn
+			// 
+			this.sıfırbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.sıfırbtn.Location = new System.Drawing.Point(12, 362);
+			this.sıfırbtn.Name = "sıfırbtn";
+			this.sıfırbtn.Size = new System.Drawing.Size(77, 88);
+			this.sıfırbtn.TabIndex = 4;
+			this.sıfırbtn.Text = "0";
+			this.sıfırbtn.UseVisualStyleBackColor = true;
+			this.sıfırbtn.Click += new System.EventHandler(this.sıfırbtn_Click);
+			// 
+			// vrglbtn
+			// 
+			this.vrglbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.vrglbtn.Location = new System.Drawing.Point(95, 362);
+			this.vrglbtn.Name = "vrglbtn";
+			this.vrglbtn.Size = new System.Drawing.Size(77, 88);
+			this.vrglbtn.TabIndex = 8;
+			this.vrglbtn.Text = ",";
+			this.vrglbtn.UseVisualStyleBackColor = true;
+			this.vrglbtn.Click += new System.EventHandler(this.vrglbtn_Click);
+			// 
+			// ikibtn
+			// 
+			this.ikibtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.ikibtn.Location = new System.Drawing.Point(95, 268);
+			this.ikibtn.Name = "ikibtn";
+			this.ikibtn.Size = new System.Drawing.Size(77, 88);
+			this.ikibtn.TabIndex = 7;
+			this.ikibtn.Text = "2";
+			this.ikibtn.UseVisualStyleBackColor = true;
+			this.ikibtn.Click += new System.EventHandler(this.ikibtn_Click);
+			// 
+			// besbtn
+			// 
+			this.besbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.besbtn.Location = new System.Drawing.Point(95, 174);
+			this.besbtn.Name = "besbtn";
+			this.besbtn.Size = new System.Drawing.Size(77, 88);
+			this.besbtn.TabIndex = 6;
+			this.besbtn.Text = "5";
+			this.besbtn.UseVisualStyleBackColor = true;
+			this.besbtn.Click += new System.EventHandler(this.besbtn_Click);
+			// 
+			// sekizbtn
+			// 
+			this.sekizbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.sekizbtn.Location = new System.Drawing.Point(95, 80);
+			this.sekizbtn.Name = "sekizbtn";
+			this.sekizbtn.Size = new System.Drawing.Size(77, 88);
+			this.sekizbtn.TabIndex = 5;
+			this.sekizbtn.Text = "8";
+			this.sekizbtn.UseVisualStyleBackColor = true;
+			this.sekizbtn.Click += new System.EventHandler(this.sekizbtn_Click);
+			// 
+			// esitbtn
+			// 
+			this.esitbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.esitbtn.Location = new System.Drawing.Point(178, 362);
+			this.esitbtn.Name = "esitbtn";
+			this.esitbtn.Size = new System.Drawing.Size(77, 88);
+			this.esitbtn.TabIndex = 12;
+			this.esitbtn.Text = "=";
+			this.esitbtn.UseVisualStyleBackColor = true;
+			this.esitbtn.Click += new System.EventHandler(this.esitbtn_Click);
+			// 
+			// ucbtn
+			// 
+			this.ucbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.ucbtn.Location = new System.Drawing.Point(178, 268);
+			this.ucbtn.Name = "ucbtn";
+			this.ucbtn.Size = new System.Drawing.Size(77, 88);
+			this.ucbtn.TabIndex = 11;
+			this.ucbtn.Text = "3";
+			this.ucbtn.UseVisualStyleBackColor = true;
+			this.ucbtn.Click += new System.EventHandler(this.ucbtn_Click);
+			// 
+			// altibtn
+			// 
+			this.altibtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.altibtn.Location = new System.Drawing.Point(178, 174);
+			this.altibtn.Name = "altibtn";
+			this.altibtn.Size = new System.Drawing.Size(77, 88);
+			this.altibtn.TabIndex = 10;
+			this.altibtn.Text = "6";
+			this.altibtn.UseVisualStyleBackColor = true;
+			this.altibtn.Click += new System.EventHandler(this.altibtn_Click);
+			// 
+			// dokuzbtn
+			// 
+			this.dokuzbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.dokuzbtn.Location = new System.Drawing.Point(178, 80);
+			this.dokuzbtn.Name = "dokuzbtn";
+			this.dokuzbtn.Size = new System.Drawing.Size(77, 88);
+			this.dokuzbtn.TabIndex = 9;
+			this.dokuzbtn.Text = "9";
+			this.dokuzbtn.UseVisualStyleBackColor = true;
+			this.dokuzbtn.Click += new System.EventHandler(this.dokuzbtn_Click);
+			// 
+			// yuzdebtn
+			// 
+			this.yuzdebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.yuzdebtn.Location = new System.Drawing.Point(416, 362);
+			this.yuzdebtn.Name = "yuzdebtn";
+			this.yuzdebtn.Size = new System.Drawing.Size(77, 88);
+			this.yuzdebtn.TabIndex = 16;
+			this.yuzdebtn.Text = "%";
+			this.yuzdebtn.UseVisualStyleBackColor = true;
+			this.yuzdebtn.Click += new System.EventHandler(this.yuzdebtn_Click);
+			// 
+			// carpbtn
+			// 
+			this.carpbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.carpbtn.Location = new System.Drawing.Point(416, 268);
+			this.carpbtn.Name = "carpbtn";
+			this.carpbtn.Size = new System.Drawing.Size(77, 88);
+			this.carpbtn.TabIndex = 15;
+			this.carpbtn.Text = "x";
+			this.carpbtn.UseVisualStyleBackColor = true;
+			this.carpbtn.Click += new System.EventHandler(this.carpbtn_Click);
+			// 
+			// artibtn
+			// 
+			this.artibtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.artibtn.Location = new System.Drawing.Point(416, 174);
+			this.artibtn.Name = "artibtn";
+			this.artibtn.Size = new System.Drawing.Size(77, 88);
+			this.artibtn.TabIndex = 14;
+			this.artibtn.Text = "+";
+			this.artibtn.UseVisualStyleBackColor = true;
+			this.artibtn.Click += new System.EventHandler(this.artibtn_Click);
+			// 
+			// temizlebtn
+			// 
+			this.temizlebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.temizlebtn.Location = new System.Drawing.Point(333, 80);
+			this.temizlebtn.Name = "temizlebtn";
+			this.temizlebtn.Size = new System.Drawing.Size(160, 88);
+			this.temizlebtn.TabIndex = 13;
+			this.temizlebtn.Text = "Temizle";
+			this.temizlebtn.UseVisualStyleBackColor = true;
+			this.temizlebtn.Click += new System.EventHandler(this.temizlebtn_Click);
+			// 
+			// artieksibtn
+			// 
+			this.artieksibtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.artieksibtn.Location = new System.Drawing.Point(333, 362);
+			this.artieksibtn.Name = "artieksibtn";
+			this.artieksibtn.Size = new System.Drawing.Size(77, 88);
+			this.artieksibtn.TabIndex = 20;
+			this.artieksibtn.Text = "+/-";
+			this.artieksibtn.UseVisualStyleBackColor = true;
+			this.artieksibtn.Click += new System.EventHandler(this.artieksibtn_Click);
+			// 
+			// bolmebtn
+			// 
+			this.bolmebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.bolmebtn.Location = new System.Drawing.Point(333, 268);
+			this.bolmebtn.Name = "bolmebtn";
+			this.bolmebtn.Size = new System.Drawing.Size(77, 88);
+			this.bolmebtn.TabIndex = 19;
+			this.bolmebtn.Text = "÷";
+			this.bolmebtn.UseVisualStyleBackColor = true;
+			this.bolmebtn.Click += new System.EventHandler(this.bolmebtn_Click);
+			// 
+			// eksibtn
+			// 
+			this.eksibtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.eksibtn.Location = new System.Drawing.Point(333, 174);
+			this.eksibtn.Name = "eksibtn";
+			this.eksibtn.Size = new System.Drawing.Size(77, 88);
+			this.eksibtn.TabIndex = 18;
+			this.eksibtn.Text = "-";
+			this.eksibtn.UseVisualStyleBackColor = true;
+			this.eksibtn.Click += new System.EventHandler(this.eksibtn_Click);
+			// 
+			// calculator
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(505, 483);
+			this.Controls.Add(this.artieksibtn);
+			this.Controls.Add(this.bolmebtn);
+			this.Controls.Add(this.eksibtn);
+			this.Controls.Add(this.yuzdebtn);
+			this.Controls.Add(this.carpbtn);
+			this.Controls.Add(this.artibtn);
+			this.Controls.Add(this.temizlebtn);
+			this.Controls.Add(this.esitbtn);
+			this.Controls.Add(this.ucbtn);
+			this.Controls.Add(this.altibtn);
+			this.Controls.Add(this.dokuzbtn);
+			this.Controls.Add(this.vrglbtn);
+			this.Controls.Add(this.ikibtn);
+			this.Controls.Add(this.besbtn);
+			this.Controls.Add(this.sekizbtn);
+			this.Controls.Add(this.sıfırbtn);
+			this.Controls.Add(this.birbtn);
+			this.Controls.Add(this.dortbtn);
+			this.Controls.Add(this.yedibtn);
+			this.Controls.Add(this.TxtBox);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "calculator";
+			this.Text = "Calculator App";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox TxtBox;
+		private System.Windows.Forms.Button yedibtn;
+		private System.Windows.Forms.Button dortbtn;
+		private System.Windows.Forms.Button birbtn;
+		private System.Windows.Forms.Button sıfırbtn;
+		private System.Windows.Forms.Button vrglbtn;
+		private System.Windows.Forms.Button ikibtn;
+		private System.Windows.Forms.Button besbtn;
+		private System.Windows.Forms.Button sekizbtn;
+		private System.Windows.Forms.Button esitbtn;
+		private System.Windows.Forms.Button ucbtn;
+		private System.Windows.Forms.Button altibtn;
+		private System.Windows.Forms.Button dokuzbtn;
+		private System.Windows.Forms.Button yuzdebtn;
+		private System.Windows.Forms.Button carpbtn;
+		private System.Windows.Forms.Button artibtn;
+		private System.Windows.Forms.Button temizlebtn;
+		private System.Windows.Forms.Button artieksibtn;
+		private System.Windows.Forms.Button bolmebtn;
+		private System.Windows.Forms.Button eksibtn;
+	}
+}
+
---
+++ CalculatorApp/CalculatorApp/calculator.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+	public partial class calculator : Form
+	{
+		private decimal valueFirst = 0.0m;
+		private decimal valueSecond = 0.0m;
+		private decimal Result = 0.0m;
+		private string operators = "+";
+		public calculator()
+		{
+			InitializeComponent();
+		}
+
+		private void sıfırbtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text =="0")
+			{
+				TxtBox.Text = "0";
+			}
+			else
+			{
+				TxtBox.Text += "0";	
+			}
+		}
+
+		private void vrglbtn_Click(object sender, EventArgs e)
+		{
+			if (!TxtBox.Text.Contains(","))
+			{
+				TxtBox.Text += ",";
+			}
+		}
+
+		private void birbtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "1";
+			}
+			else
+			{
+				TxtBox.Text += "1";
+			}
+		}
+
+		private void ikibtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "2";
+			}
+			else
+			{
+				TxtBox.Text += "2";
+			}
+		}
+
+		private void ucbtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "3";
+			}
+			else
+			{
+				TxtBox.Text += "3";
+			}
+		}
+
+		private void dortbtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "4";
+			}
+			else
+			{
+				TxtBox.Text += "4";
+			}
+		}
+
+		private void besbtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "5";
+			}
+			else
+			{
+				TxtBox.Text += "5";
+			}
+		}
+
+		private void altibtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "6";
+			}
+			else
+			{
+				TxtBox.Text += "6";
+			}
+		}
+
+		private void yedibtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "7";
+			}
+			else
+			{
+				TxtBox.Text += "7";
+			}
+		}
+
+		private void sekizbtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "8";
+			}
+			else
+			{
+				TxtBox.Text += "8";
+			}
+		}
+
+		private void dokuzbtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text == "0")
+			{
+				TxtBox.Text = "9";
+			}
+			else
+			{
+				TxtBox.Text += "9";
+			}
+		}
+
+		private void artieksibtn_Click(object sender, EventArgs e)
+		{
+			if (TxtBox.Text.Contains("-"))
+			{
+				TxtBox.Text = TxtBox.Text.Trim('-');
+			}
+			else
+			{
+				TxtBox.Text = "-" + TxtBox.Text;
+			}
+		}
+
+		private void eksibtn_Click(object sender, EventArgs e)
+		{
+			valueFirst=decimal.Parse(TxtBox.Text);
+			TxtBox.Clear();
+			operators = "-";
+		}
+
+		private void artibtn_Click(object sender, EventArgs e)
+		{
+			valueFirst = decimal.Parse(TxtBox.Text);
+			TxtBox.Clear();
+			operators = "+";
+		}
+
+		private void bolmebtn_Click(object sender, EventArgs e)
+		{
+			valueFirst = decimal.Parse(TxtBox.Text);
+			TxtBox.Clear();
+			operators = "/";
+		}
+
+		private void carpbtn_Click(object sender, EventArgs e)
+		{
+			valueFirst = decimal.Parse(TxtBox.Text);
+			TxtBox.Clear();
+			operators = "*";
+		}
+
+		private void yuzdebtn_Click(object sender, EventArgs e)
+		{
+			valueFirst = decimal.Parse(TxtBox.Text);
+			TxtBox.Clear();
+			operators = "%";
+		}
+
+		private void esitbtn_Click(object sender, EventArgs e)
+		{
+			switch (operators) 
+			{
+				case "-":
+					valueSecond = decimal.Parse(TxtBox.Text);
+					Result=valueFirst-valueSecond;
+					TxtBox.Text= Result.ToString();
+					break;
+				case "+":
+					valueSecond = decimal.Parse(TxtBox.Text);
+					Result = valueFirst + valueSecond;
+					TxtBox.Text = Result.ToString();
+					break;
+				case "*":
+					valueSecond = decimal.Parse(TxtBox.Text);
+					Result = valueFirst * valueSecond;
+					TxtBox.Text = Result.ToString();
+					break;
+				case "/":
+					valueSecond = decimal.Parse(TxtBox.Text);
+					Result = valueFirst / valueSecond;
+					TxtBox.Text = Result.ToString();
+					break;
+				case "%":
+					valueSecond = decimal.Parse(TxtBox.Text);
+					Result = valueFirst % valueSecond;
+					TxtBox.Text = Result.ToString();
+					break;
+			}
+		}
+
+		private void temizlebtn_Click(object sender, EventArgs e)
+		{
+			valueFirst = 0.0m;
+			valueSecond = 0.0m;
+			TxtBox.Text = "0";
+		}
+	}
+}
